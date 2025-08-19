eb920494f1021ad1e823b45fe2684ab3038f13c3
Byron Pillay <byronpillay2704@gmail.com>
2025-08-19 08:58:41 +0200

    Add files via upload

--- ConsensusProtocol.java
+++ ConsensusProtocol.java
@@ -1,16 +1,26 @@
 import java.util.Vector;
 
-public abstract class ConsensusProtocol{
+public abstract class ConsensusProtocol {
 
-    private int threadCount;
+	private int threadCount;
 	protected Vector<Integer> proposed;
 
-	public ConsensusProtocol(int threadCount)	{
-		//TODO: Implement    
+	public ConsensusProtocol(int threadCount) {
+		// done
+		this.threadCount = threadCount;
+		this.proposed = new Vector<>(threadCount);
+
+		for (int i = 0; i < threadCount; i++) {
+			proposed.add(null);
+		}
 	}
 
-	void propose(int threadId, int value)	{
-		//TODO: Implement function   
+	void propose(int threadId, int value) {
+		// done
+		if (threadId < 0 || threadId >= threadCount) {
+			throw new IllegalArgumentException("Invalid threadId");
+		}
+		proposed.set(threadId, value);
 	}
 
 	public abstract int decide(int id, int value);
--- FIFOConsensus.java
+++ FIFOConsensus.java
@@ -1,16 +1,36 @@
 
+import java.lang.reflect.Method;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class FIFOConsensus extends ConsensusProtocol {
     private Queue<Integer> queue = new LinkedList<>();
-	
-	public FIFOConsensus(int threadCount){
-		//TODO: Implement
-	}
+
+    public FIFOConsensus(int threadCount) {
+        // done?
+        super(threadCount);
+    }
 
     @Override
     public int decide(int threadId, int value) {
-        //TODO: Implement function   
+        // done
+
+        // //METHOD 1: First unique value wins.
+        // propose(threadId, value);
+
+        // if(!queue.contains(value)){
+        // queue.add(value);
+        // }
+
+        // return queue.peek();
+
+        // Method 2: First threads value wins
+        propose(threadId, value);
+
+        if (queue.isEmpty()) {
+            queue.add(value);
+        }
+
+        return queue.peek();
     }
 }
